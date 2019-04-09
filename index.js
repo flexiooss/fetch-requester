ec37a6d72b716a9cc84d2fff07268572157b258c
TomAchT <thomas@flexio.fr>
2019-04-09 05:42:19 +0200

    init requester

---
+++ index.js
@@ -0,0 +1,1 @@
+export {FetchRequester} from './src/js/FetchRequester'
---
+++ src/test/FetchRequester.test.js
@@ -0,0 +1,16 @@
+/* global runTest */
+import {FetchRequester} from '../js/FetchRequester'
+import {TestCase} from 'code-altimeter-js'
+const assert = require('assert')
+
+export class TestFetchRequester extends TestCase {
+  testPath() {
+    new Headers()
+    // const requester = new FetchRequester().path('https://www.fakeApi.com').get()
+    // assert.throws(() => {
+    //   const requester = new FetchRequester().path('bibi').get()
+    // })
+  }
+}
+
+runTest(TestFetchRequester)
